49267b691eecc6390abbe4f1ba2b5714a12d6aa0
hthomp15 <hailethomp@gmail.com>
2022-05-15 15:25:20 -0400

    add header and nav bar

--- haile-2.0/src/components/Nav/index.js
+++ haile-2.0/src/components/Nav/index.js
@@ -1,14 +1,30 @@
 import React from "react";
+import { useState } from "react";
 
 function Nav() {
+    const navBars = [
+        {name: "About",link: "#about"},
+        {name: "Contact", link: "#contact" },
+        {name: "Work", link: "#work" }, 
+        {name: "Resume", link: "#resume", target: "_blank" },
+    ];
+
+    const [isOpen, setIsOpen] = useState(false);
+
+
 
     return(
-        <nav class="navigation">
-            <ul class="nav-bar">
-                <li><a href="#about-me">About Me</a></li>
-                <li><a href="#my-work">Work</a></li>
-                <li><a href="#contact-me">Contact Me</a></li>
-                <li><a href="Assets/Hthompson-Resume-2022.pdf" target="_blank">Resume</a></li>
+        <nav className="navigation">
+            <h1>Hello I'm Haile Thompson</h1>
+            <ul className="nav-bar">
+                {navBars.map((navBar, index) => {
+                    return(
+                    <li className={isOpen ? "nav-closed" : "nav-active"} key={index}> 
+                        <a href={navBar.link} target={navBar.target} onClick={ ()=>{
+                            setIsOpen(!isOpen);
+                        }}>{navBar.name}</a>
+                    </li>)
+                })}           
             </ul>
         </nav> 
     )
