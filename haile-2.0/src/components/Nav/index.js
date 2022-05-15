e91789c501fcdcbf681b00ecd01db2934439b90a
hthomp15 <hailethomp@gmail.com>
2022-05-15 12:46:30 -0400

    remove react default code. Add some starter styles and nav

---
+++ haile-2.0/src/components/Nav/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Nav() {
+
+    return(
+        <nav class="navigation">
+            <ul class="nav-bar">
+                <li><a href="#about-me">About Me</a></li>
+                <li><a href="#my-work">Work</a></li>
+                <li><a href="#contact-me">Contact Me</a></li>
+                <li><a href="Assets/Hthompson-Resume-2022.pdf" target="_blank">Resume</a></li>
+            </ul>
+        </nav> 
+    )
+}
+
+export default Nav;
