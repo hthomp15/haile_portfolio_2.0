c8e87ad0b7ac41962c62831a21fcbbcb00f47a85
hthomp15 <hailethomp@gmail.com>
2022-05-15 22:45:54 -0400

    add footer SVG's

--- haile-2.0/src/App.js
+++ haile-2.0/src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/index.js';
 import About from './components/About/index.js';
+import Projects from './components/Projects/index.js';
 import Footer from './components/Footer/index.js';
 
 function App() {
+  // const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
         <Header></Header>
         <main>
           <About></About>
-          {/* <Projects></Projects> */}
+          <Projects></Projects>
+          {/* {!isOpen? (<About></About>) : (<Projects></Projects>)} */}
         </main>
         <Footer></Footer>
     </div>
